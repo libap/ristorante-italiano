1affadfaa1e799c1d625df850afdfa9f9eefab08
libap <118845814+libap@users.noreply.github.com>
2023-06-26 00:47:32 +0200

    dynamique plats du jour

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,3 +169,77 @@ window.addEventListener("mousemove", function (event) {
 
 });
 
+const menuItems = [
+  {
+    title: 'Rotolos',
+    day: '',
+    price: '8,50€',
+    description: "Un rouleau de pâtes farci d'épinards et de ricotta.",
+    img: '../assets/images/plats_du_jours/rotolo-di-pasta.png'
+  },
+  {
+    title: 'Lasagnes saumon épignard',
+    day: 'DIMANCHE',
+    price: '8,50€',
+    description: 'Vegetables, cheeses, ground meats, tomato sauce, seasonings and spices',
+    img: '../assets/images/desserts/charlotte_fruits_rouges.jpg'
+  },
+  {
+    title: "Souris d'agneau",
+    day: 'LUNDI',
+    price: '14,50€',
+    description: 'Une pièce tendre et savoureuse de viande d\'agneau cuite lentement, offrant une texture fondante et des saveurs riches.',
+    img: '../assets/images/desserts/moelleux_chocolat.jpg'
+  },
+  {
+    title: 'Tartelette fraise frangipane',
+    day: '',
+    price: '39.00€',
+    description: 'Vegetables, cheeses, ground meats, tomato sauce, seasonings and spices.',
+    img: '../assets/images/desserts/tartelettes_fraise_frangipane.jpg'
+  },
+  {
+    title: 'Tiramisu',
+    day: '',
+    price: '25.00€',
+    description: 'Avocados with crab meat, red onion, crab salad stuffed red bell pepper and green bell pepper.',
+    img: '../assets/images/desserts/tiramisu.jpg'
+  }
+];
+
+// Obtenir le jour actuel en français
+const joursSemaine = ['DIMANCHE', 'LUNDI', 'MARDI', 'MERCREDI', 'JEUDI', 'VENDREDI', 'SAMEDI'];
+const date = new Date();
+const jourActuel = joursSemaine[date.getDay()];
+
+// Générer le code HTML avec les éléments du tableau menuItems
+let html = '';
+menuItems.forEach((menuItem) => {
+  if (menuItem.day === '' || menuItem.day.toUpperCase() === jourActuel) {
+    const badgeText = (menuItem.day !== '') ? 'Plat du jour' : menuItem.day;
+    html += `
+      <li>
+        <div class="menu-card hover:card">
+          <figure class="card-banner img-holder" style="--width: 100; --height: 100;">
+            <img src="${menuItem.img}" width="100" height="100" loading="lazy" alt="${menuItem.title}" class="img-cover">
+          </figure>
+          <div>
+            <div class="title-wrapper">
+              <h3 class="title-3">
+                <a href="#" class="card-title">${menuItem.title}</a>
+              </h3>
+              <span class="badge label-1">${badgeText}</span>
+              <span class="span title-2">${menuItem.price}</span>
+            </div>
+            <p class="card-text label-1">
+              ${menuItem.description}
+            </p>
+          </div>
+        </div>
+      </li>`;
+  }
+});
+
+// Insérer le code HTML dans l'élément avec la classe "grid-list plats"
+const gridListPlats = document.querySelector('.grid-list.plats');
+gridListPlats.innerHTML = html;
